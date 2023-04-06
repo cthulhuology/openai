7fd05561c2078a5c9779ee68e19017f2b0068d6d
David Goehrig <dave@dloh.org>
2023-04-06 23:47:29 +0200

    Updating copyright notice and license

--- src/openai.erl
+++ src/openai.erl
@@ -1,5 +1,26 @@
+%%
+%% MIT No Attribution  
+%% Copyright 2023 David J Goehrig <dave@dloh.org>
+%%
+%% Permission is hereby granted, free of charge, to any person obtaining a copy 
+%% of this software and associated documentation files (the "Software"), to 
+%% deal in the Software without restriction, including without limitation the 
+%% rights to use, copy, modify, merge, publish, distribute, sublicense, and/or 
+%% sell copies of the Software, and to permit persons to whom the Software is 
+%% furnished to do so.  
+%%
+%% THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 
+%% IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 
+%% FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 
+%% AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 
+%% LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
+%% FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS 
+%% IN THE SOFTWARE.
+
 
 -module(openai).
+-author({ "David J Goehrig", "dave@dloh.org"}).
+-copyright(<<"© 2023 David J. Goehrig"/utf8>>).
 -behavior(gen_server).
 -export([ start_link/0, models/0 ]).
 -export([ init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3, engines/0, engine/1 ]).
